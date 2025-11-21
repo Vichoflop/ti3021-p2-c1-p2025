7d2b87fa686345ee7422ef68e841c92e50e0aea3
Vichoflop <vicente.chandia03@inacapmail.cl>
2025-11-20 23:19:34 -0300

    actualizacion_20_11_2025_23:19

--- oracle_ejemplo/schema.sql
+++ oracle_ejemplo/schema.sql
@@ -1,72 +1,30 @@
-CREATE TABLE Usuarios(
-    id_usuario INTEGER PRIMARY KEY,
-    nombres VARCHAR2(100) NOT NULL,
-    apellidos VARCHAR2(100) NOT NULL,
-    Rut VARCHAR2(12) UNIQUE NOT NULL,
-    correo VARCHAR2(50) UNIQUE NOT NULL
 );
 
 
-CREATE TABLE Estudiantes(
-    id_estudiante INTEGER PRIMARY KEY,
-    id_usuario INTEGER,
     PrestamosActivos INTEGER NOT NULL,
     EstadoDeuda VARCHAR2(50) NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
 );
 
 
-CREATE TABLE Docentes(
-    id_docente INTEGER PRIMARY KEY,
-    id_usuario INTEGER,
-    MaterialExclusivoAccedido VARCHAR2(50) NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
 );
 
 
-CREATE TABLE Investigadores(
-    id_investigador INTEGER PRIMARY KEY,
-    NivelAcceso VARCHAR2(50) NOT NULL
 );
 
 
-CREATE TABLE Libros(
-    id_libro INTEGER PRIMARY KEY,
-    id_estudiante INTEGER,
-    nombre VARCHAR2(50),
-    autor VARCHAR2(50),
-    anio_publicacion NUMBER(4),
-    CantidadPaginas INTEGER,
-    Cantidad INTEGER,
-    Descripcion VARCHAR2(100),
-    FOREIGN KEY (id_estudiante) REFERENCES Estudiantes(id_estudiante)
 );
 
 
-CREATE TABLE Prestamos(
-    Id_prestamo INTEGER PRIMARY KEY,
-    id_estudiante INTEGER NOT NULL,
-    Id_libro INTEGER NOT NULL,
-    cantidad INTEGER NOT NULL,
-    fecha_prestamo DATE,
-    fecha_devolucion DATE,
-    FOREIGN KEY (id_estudiante) REFERENCES Estudiantes(id_estudiante),
-    FOREIGN KEY (Id_libro) REFERENCES Libro(id_libro)
 );
 
 
 CREATE TABLE DataSetsDescargados(
-    id_Data_Set_Descargado INTEGER PRIMARY KEY,
-    id_investigador INTEGER NOT NULL,
-    Nombre VARCHAR2(50) NOT NULL,
-    Cantidad INTEGER NOT NULL,
     FOREIGN KEY (id_investigador) REFERENCES Investigadores(id_investigador)
 );
 
 
 CREATE TABLE Biblioteca(
-    id_Biblioteca INTEGER PRIMARY KEY,
-    CantidadMaterial INTEGER NOT NULL,
-    GestionPrestamo INTEGER NOT NULL
 );
 
